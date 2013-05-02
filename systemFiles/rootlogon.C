00c7e1d24fec0273cd38e49365e648533bca367f
Alexis Schubert <alexis4@stanford.edu>
2013-05-02 15:57:29 -0700

    adding root logon script

---
+++ systemFiles/rootlogon.C
@@ -0,0 +1,12 @@
+{
+
+  cout << "this is alexis' rootlogon.C" << endl;
+
+  gROOT->SetStyle("Plain");
+  gStyle->SetOptStat(0);
+  gStyle->SetPalette(1);
+
+  // use consistent number of significant digits:
+  gStyle->SetStripDecimals(kFALSE);
+
+}
