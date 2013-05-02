df79892999ca5f466da2de92e4efcc4137a8275d
Alexis Schubert <alexis4@stanford.edu>
2013-05-02 16:01:32 -0700

    added comments

--- systemFiles/rootlogon.C
+++ systemFiles/rootlogon.C
@@ -2,11 +2,11 @@
 
   cout << "this is alexis' rootlogon.C" << endl;
 
-  gROOT->SetStyle("Plain");
-  gStyle->SetOptStat(0);
-  gStyle->SetPalette(1);
+  gROOT->SetStyle("Plain");     
+  gStyle->SetOptStat(0);        // suppress box on plots
+  gStyle->SetPalette(1);        // rainbow palette
 
-  // use consistent number of significant digits:
+  // use consistent number of decimal places in plots:
   gStyle->SetStripDecimals(kFALSE);
 
 }
