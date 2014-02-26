33a9364d0616f62e8765d76325a1b46e0c3458fe
alexis-s <alexis4@stanford.edu>
2014-02-26 10:26:23 -0800

    added commands to remove hist title border

--- systemFiles/rootlogon.C
+++ systemFiles/rootlogon.C
@@ -5,6 +5,8 @@
   gROOT->SetStyle("Plain");     
   gStyle->SetOptStat(0);        // suppress box on plots
   gStyle->SetPalette(1);        // rainbow palette
+  gStyle->SetTitleStyle(0);     // added 25 Feb 2014
+  gStyle->SetTitleBorderSize(0);        // added 25 Feb 2014       
 
   // use consistent number of decimal places in plots:
   gStyle->SetStripDecimals(kFALSE);
