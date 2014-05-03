497ba112a914e9c1d0c7677e38fab74411965ae0
Alexis Schubert <alexis4@stanford.edu>
2014-05-03 14:23:33 -0700

    now working at SLAC

--- testScripts/mcComparisonScript.py
+++ testScripts/mcComparisonScript.py
@@ -22,6 +22,7 @@ gROOT.SetBatch(True)
 from ROOT import TH1D
 from ROOT import TFile
 from ROOT import TCanvas
+from ROOT import TLegend
 
 
 
@@ -57,40 +58,52 @@ def process_directory(
     print "--> processing directory:", directory
 
 
-    # these hists will get created (hist title, draw string, selection):
-    hist_lists = []
-    hist_lists.append(("Energy deposited in LXe sum", "fMonteCarloData.fTotalEnergyInLiquidXe", "")) 
-
-    # this is not really calibrated!
-    hist_lists.append(("Sum Raw Energy Clusters", "Sum$(fChargeClusters.fRawEnergy)", ""))
-    hist_lists.append(("PCD Sum energy", "Sum$(fMonteCarloData.fPixelatedChargeDeposits.fTotalEnergy)*1e3", ""))
-
-
+    # create a dict of info for each hist to be created:
     hist_info = []
 
+    hist_info.append({
+      "title": "Energy deposited in LXe sum", 
+      "draw_string": "fMonteCarloData.fTotalEnergyInLiquidXe", 
+    }) 
+
+    # this is not really calibrated!
+    hist_info.append({
+      "title": "Sum Raw Energy Clusters", 
+      "draw_string": "Sum$(fChargeClusters.fRawEnergy)", 
+    }) 
+
+    hist_info.append({
+      "title": "PCD Sum energy", 
+      "draw_string": "Sum$(fMonteCarloData.fPixelatedChargeDeposits.fTotalEnergy)*1e3", 
+    }) 
+
+    # FIXME -- selection isn't being used
+    # hist, draw string
+
     # loop over hist_lists, create empty hists with the specified info:
-    for i, hist_list in enumerate(hist_lists):
+    for i, hist_dict in enumerate(hist_info):
 
-        (title, draw_string, selection) = hist_list
-        name = "hist%i" % i
-
+        name = "hist%i_dir%s" % (i, suffix)
+        hist_dict["name"] = name
         
-        title = "%s: %s" % (title, draw_string)
-        if selection != "": title += " {%s}" % selection
+        title = "%(title)s: %(draw_string)s" % hist_dict
         hist = create_hist(name, title)
-        hist_info.append((hist, draw_string))
-
-    for hist_list in hist_lists:
-        print hist_list
+        hist_dict["hist"] = hist
+        #print hist_dict
 
 
     root_filenames = glob.glob("%s/*.root" % directory)
     print "%i files" % len(root_filenames)
 
 
-    for root_filename in root_filenames:
+    for (i_file, root_filename) in enumerate(root_filenames):
 
-        print "\t processing", root_filename
+        print "\t processing file %i of %i: %s" % (
+            i_file, 
+            len(root_filenames),
+            root_filename,
+        )
+
         try:
             root_file = TFile(root_filename)
             tree = root_file.Get("tree")
@@ -100,44 +113,73 @@ def process_directory(
             print "BAD FILE"
             continue
 
-        hist_info[0][0].GetDirectory().cd()
+        tree.GetEntry(0)
+        svn_revision = tree.EventBranch.fEventHeader.fSVNRevision
+        build_id = tree.EventBranch.fEventHeader.fBuildID
+
+        if i_file == 0:
+            prev_svn_revision = svn_revision
+        if svn_revision != prev_svn_revision:
+            print "SVN version changed!!"
+        #print "\t\t", svn_revision, build_id
+
+        hist_info[0]["hist"].GetDirectory().cd()
 
 
-        for (hist, draw_string) in hist_info:
+        for hist_dict in hist_info:
 
-            name = hist.GetName()
-            tree.Draw("%s >> +%s" % (draw_string, name))
-            print "\t\t%i entries in %s: %s" % (hist.GetEntries(), name, hist.GetTitle())
+            draw_string = "%(draw_string)s >>+ %(name)s" % hist_dict
+            #print draw_string
+            tree.Draw(draw_string)
+            #print "\t\t%i entries in %(name)s: %(title)s" % (n_entries, hist_dict)
+            hist_dict["svn_revision"] = svn_revision
+            hist_dict["build_id"] = build_id
+            hist_dict["n_entries"] = hist.GetEntries()
+
+        if i_file > 500: # debugging
+            print "====> STOPPING AT FILE %s!" % i_file
+            break 
 
 
-    hists = []
+    for hist_dict in hist_info:
 
-        print "--> scaling %s" % hist.GetName()
+        print "--> scaling %(name)s" % hist_dict
+        hist = hist_dict["hist"]
         hist.Scale(1.0/hist.GetEntries())
-        hists.append(hist)
+        hist_dict["n_entries"] = hist.GetEntries()
 
-    return hists
+    return hist_info
 
 
 def main(directory1, directory2):
 
-    hists1 = process_directory(directory1, "1")
-    hists2 = process_directory(directory2, "2")
+    hist_info1 = process_directory(directory1, "1")
+    hist_info2 = process_directory(directory2, "2")
 
 
     canvas = TCanvas("canvas", "")
+    canvas.SetTopMargin(0.2)
     canvas.SetLogy(1)
 
-    for i in xrange(len(hists1)):
+    for i in xrange(len(hist_info1)):
 
         print "hists", i
 
-        hist1 = hists1[i]
-        hist2 = hists2[i]
+        hist_dict1 = hist_info1[i] 
+        hist_dict2 = hist_info2[i] 
+
+        hist1 = hist_dict1["hist"]
+        hist2 = hist_dict2["hist"]
+
+        legend = TLegend(0.1, 0.8, 0.9, 0.9)
+        legend_entry = "%(build_id)s, %(n_entries).1e entries"
+        legend.AddEntry(hist1, legend_entry % hist_dict1, "l")
+        legend.AddEntry(hist2, legend_entry % hist_dict2, "l")
 
         hist1.Draw()
         hist2.SetLineColor(2)
         hist2.Draw("same")
+        legend.Draw()
 
         canvas.Update()
         canvas.Print("%s.pdf" % hist1.GetName())
