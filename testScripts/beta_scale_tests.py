34942e5a8807a203e10c72a625961e9322d16998
Alexis Schubert <alexis4@stanford.edu>
2014-07-18 11:07:13 -0700

    commenting out beta, compton stuff

--- testScripts/beta_scale_tests.py
+++ testScripts/beta_scale_tests.py
@@ -46,24 +46,24 @@ legend.SetFillColor(0)
 legend.SetNColumns(2)
 
 legend.AddEntry(hist_all, "All counts", "l")
-legend.AddEntry(hist_compt, "compton", "l")
+#legend.AddEntry(hist_compt, "compton", "l")
 legend.AddEntry(hist_pp, "pair prod", "l")
-legend.AddEntry(hist_pe, "pe?", "l")
+#legend.AddEntry(hist_pe, "pe?", "l")
 
 print "beta:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe >> hist_beta",
 "Sum$(fMonteCarloData.fPixelatedChargeDeposits.fBetaEnergy)>0")
 
 print "all:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe >> hist_all", "")
 
-print "compt:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe >> hist_compt",
-"Sum$(fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)>0", "same")
+#print "compt:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe >> hist_compt",
+#"Sum$(fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)>0", "same")
 
 print "pp:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe >> hist_pp",
 "Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy)>0", "same")
 
-print "pe:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe >> hist_pe",
-"(Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy+fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)==0)", 
-"same")
+#print "pe:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe >> hist_pe",
+#"(Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy+fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)==0)", 
+#"same")
 #"Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy)==0 && Sum$(fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)==0", 
 
 legend.Draw()
@@ -83,14 +83,14 @@ tree.SetLineColor(TColor.kRed)
 print "pp_ss:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe",
 "Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy)>0 && @fChargeClusters.size()==1", "same")
 
-tree.SetLineColor(TColor.kBlue)
-print "compt_ss:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe",
-"Sum$(fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)>0 && @fChargeClusters.size()==1", "same")
+#tree.SetLineColor(TColor.kBlue)
+#print "compt_ss:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe",
+#"Sum$(fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)>0 && @fChargeClusters.size()==1", "same")
 
-tree.SetLineColor(TColor.kGreen+1)
-print "pe_ss:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe", 
-"(Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy+fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)==0) && (@fChargeClusters.size()==1)", 
-"same")
+#tree.SetLineColor(TColor.kGreen+1)
+#print "pe_ss:", tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe", 
+#"(Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy+fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)==0) && (@fChargeClusters.size()==1)", 
+#"same")
 #"(Sum$(fMonteCarloData.fPixelatedChargeDeposits.fConversionEnergy)==0) && (Sum$(fMonteCarloData.fPixelatedChargeDeposits.fComptonEnergy)==0) && (@fChargeClusters.size()==1)", 
 
 #print tree.Draw("fMonteCarloData.fTotalEnergyInLiquidXe",
