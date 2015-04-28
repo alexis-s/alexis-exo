0e12dd12530a48816fcb917ff743d8f9263f6f5e
alexis-s <alexis4@stanford.edu>
2015-04-27 18:59:36 -0700

    changes from hopper

--- testScripts/submitPythonJobsNERSC.py
+++ testScripts/submitPythonJobsNERSC.py
@@ -12,37 +12,28 @@ arguments:
 
 import os
 import sys
+from optparse import OptionParser
 
-#options:
-queue = "regular" 
-hours = 1
-minutes = 30
-do_debug = True
+def process_files(python_script, filenames, nparallel):
 
-if do_debug:
-  queue = "debug" 
-  hours = 0
-  minutes = 10
+    #options:
+    queue = "regular" 
+    hours = 1
+    minutes = 30
+    do_debug = False # debugging
 
-python_script = sys.argv[1]
-filenames = sys.argv[2:]
+    if do_debug:
+        queue = "debug" 
+        hours = 0
+        minutes = 10
 
-#print "\n".join(filenames)
-#filenames.sort()
-#print "\n".join(filenames)
-#sys.exit()
+    for filename in filenames:
+        print "--> processing:", python_script, filename
 
+        basename = os.path.basename(filename)
+        print "basename:", basename
 
-print 'python_script:', python_script
-
-
-for filename in filenames:
-    print "--> processing:", python_script, filename
-
-    basename = os.path.basename(filename)
-    print "basename:", basename
-
-    qsub_cmd = """
+        qsub_cmd = """
 qsub \\
   -q %(queue)s \\
   -l walltime=%(hours)02i:%(minutes)02i:00 \\
@@ -50,32 +41,58 @@ qsub \\
   -d %(cwd)s \\
   -V \\
 """ % {
-      "queue": queue,
-      "hours": hours,
-      "minutes": minutes,
-      "name": basename,
-      "cwd": os.getcwd(),
-      }
+          "queue": queue,
+          "hours": hours,
+          "minutes": minutes,
+          "name": basename,
+          "cwd": os.getcwd(),
+          }
 
 
-    cmd = "printenv \n"
-    cmd += "aprun -n 1 python ./%(python_script)s %(filename)s \n"  % {
-      "python_script": python_script,
-      "filename": filename,
-    }
-    cmd += "done"
-    #script += "'printenv; aprun -n 1 python "+python_script+" "+filename+" '"
+        cmd = "printenv \n"
+        #cmd += "aprun -n 1 python ./%(python_script)s %(filename)s \n"  % {
+        cmd += "aprun -n 1 python %(python_script)s %(filename)s \n"  % {
+          "python_script": python_script,
+          "filename": filename,
+        }
+        cmd += "done"
+        #script += "'printenv; aprun -n 1 python "+python_script+" "+filename+" '"
 
-    print qsub_cmd
-    print cmd
+        print qsub_cmd
+        print cmd
 
-    write_handle = os.popen(qsub_cmd, 'w')
-    write_handle.write(cmd)
-    write_handle.close()
+        write_handle = os.popen(qsub_cmd, 'w')
+        write_handle.write(cmd)
+        write_handle.close()
 
-    if do_debug:
-        print "==> debugging! only running first job!!"
-        break
+        if do_debug:
+            print "==> debugging! only running first job!!"
+            break
+
+
+if __name__ == "__main__":
+
+    usage = "%prog [options] python_script files_to_process"
+    parser = OptionParser(usage)
+
+    parser.add_option("--nparallel",dest="nparallel",type="int",default=1,
+        help="set number of jobs to submit at the same time [Default = 5]")
+    options, args = parser.parse_args()
+
+    if len(args) < 2:
+        parser.print_help()
+        parser.error("Wrong number of arguments!")
+
+    python_script = args[0]
+    filenames = args[1:]
+    print 'python_script:', python_script
+
+    process_files(python_script, filenames, options.nparallel)
+
+    #print "\n".join(filenames)
+    #filenames.sort()
+    #print "\n".join(filenames)
+    #sys.exit()
 
 
 
